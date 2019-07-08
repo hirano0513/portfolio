221fba6051ced19a4c4980673127a982487d484d
hirano0513 <shimaimo@gmail.com>
2019-07-08 20:50:56 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.11.0'
 set :application, 'portfolio'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git://github.com/hirano0513/portfolio.git'
+set :repo_url,  'git@github.com:hirano0513/portfolio.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
